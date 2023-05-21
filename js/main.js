357c73ca02c1ffe2d0f594310b1271c292e521c8
manuel oubina <omdigital.ar@gmail.com>
2023-05-21 19:55:12 -0300

    segunda entrega del proyecto

--- js/main.js
+++ js/main.js
@@ -1,20 +1,40 @@
 const calcularEnvio = () =>{
     let valorPedido;
     do { 
-        valorPedido = parseInt(prompt("Inserte el valor final de tu pedido"));
+        valorPedido = parseInt(prompt("Inserte el valor final de tu pedido, para saber si tiene que abonar envío"));
         if (!parseInt(valorPedido)) {
             alert("Inserte un monto válido")
             console.log(valorPedido)
         }
         else {
             if(valorPedido >= 15000){
-            return alert("Tienes envío gratis");
-            }else{return alert("El valor de tu envío es $2000")}
+            alert("¡Felicitaciones! Tienes envío gratis.");
+            }else {return alert('Deberás abonar tu envīo. Vamos a calcular el precio en base al peso del paquete.')}
             }
     }while (valorPedido != NaN)
-}
 
-calcularEnvio()
+}
+calcularEnvio();
+
+
+
+const precioSegunValor = () =>{
+    let pesoPedido = prompt("Ingrese por favor el peso en kg de su paquete");
+    const encontrado = valorEnvios.find(item => item.peso >= pesoPedido);
+    alert("El valor a abonar es de $" + encontrado.precio + ".");
+    alert("Gracias por comprar en Cyberwax.");
+};
+
+const valorEnvios = [
+    { peso: 1, precio: 1000 },
+    { peso: 2, precio: 1300 },
+    { peso: 5, precio: 1500 },
+    { peso: 10, precio: 2500 },
+    { peso: 20, precio: 3500 }, 
+];
+
+precioSegunValor();
+
 
 
 
